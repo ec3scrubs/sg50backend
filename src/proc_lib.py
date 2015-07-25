7af865b92343a9a5a39f391e2cfd2545ba62b44a
Mark Lee <markleechuyong@gmail.com>
2015-07-25 18:53:35 +0800

    updated query fx

--- src/proc_lib.py
+++ src/proc_lib.py
@@ -1,17 +1,32 @@
 __author__ = 'mark'
 
 import sqlite3 as lite
+from geopy.distance import vincenty
 
-def query_sql(type):
+home = (1.3725179, 103.83279)
+
+def query_data(location,type):
     DATABASE = ('../data/data.db')
     type = type.lower()
     con = lite.connect(DATABASE)
+    data = []
+    res = []
     with con:
         cur = con.cursor()
         cur.execute("SELECT * FROM data WHERE type='"+type+"';")
-        res = cur.fetchall()
-        print res
+        data = cur.fetchall()
+
+    tmp = []
+    for row in data:
+        c_loc = (row[2], row[3])
+        tmp.append((row[0],vincenty(location, c_loc).km))
+    tmp = sorted(tmp, key=lambda item:item[1])
+    tmp = tmp[:10]
+    res = tmp
+    return res
 
 
 if __name__ == "__main__":
-    query_sql(type="school")
+    res = query_data(location=home,type="school")
+    for (x,y) in res:
+        print x, y
